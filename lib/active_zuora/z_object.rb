d4ad4cb8d6cba0bc571016644d1c970134c37e67
Ed Lebert <edlebert@gmail.com>
2012-05-29 22:50:26 -0500

    added equality operator to z_objects

--- lib/active_zuora/z_object.rb
+++ lib/active_zuora/z_object.rb
@@ -10,6 +10,12 @@ module ActiveZuora
       include Persistence
       field :id, :string
     end
+ 
+    def ==(another_zobject)
+      another_zobject.is_a?(ZObject) &&
+        zuora_object_name == another_zobject.zuora_object_name && 
+        id == another_zobject.id
+    end
 
   end
 end
