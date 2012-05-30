22830cbcda4c28198f86f0940fd28ec84c950119
Ed Lebert <edlebert@gmail.com>
2012-05-29 22:49:13 -0500

    added simple subscription integration test

--- spec/active_zuora/fields/field_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe ActiveZuora::Field do
-
-  it "should work" do
-    nil.should be_nil
-  end
-
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
 require 'rubygems'
 require 'rspec'
 require 'active_zuora'
+
+ActiveZuora.configure(
+  :log => ENV['DEBUG'],
+  :username => ENV['ZUORA_USER'],
+  :password => ENV['ZUORA_PASS']
+)
+
+module Z; end
+ActiveZuora.generate_classes :module => Z
