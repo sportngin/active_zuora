05f2eff84fe70857d94483bb77857f4ae67b4690
timsegraves <timsegraves@gmail.com>
2013-02-13 10:58:04 -0600

    Adding temporary debugging

--- lib/active_zuora/persistence.rb
+++ lib/active_zuora/persistence.rb
@@ -93,10 +93,13 @@ module ActiveZuora
 
         # Make calls in batches of 50
         new_objects.each_sice(50) do |batch|
+          puts "AAAA"
+          puts batch.length
           new_records += process_save(batch, :create)
         end
 
         updated_objects.each_sice(50) do |batch|
+          puts batch.length
           updated_records += process_save(batch, :update)
         end
 
