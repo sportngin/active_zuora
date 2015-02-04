80d7659d796e0ec06b43288214a41ee01e4c9782
Manic Chuang <manic.csie90@gmail.com>
2015-02-04 11:05:04 +1100

    Extract method and add unit test

--- lib/active_zuora/base.rb
+++ lib/active_zuora/base.rb
@@ -39,20 +39,20 @@ module ActiveZuora
       attributes = options.delete(:force_type) ? 
         { "xsi:type" => "#{qualifier}:#{zuora_object_name}" } : {}
 
-      fields_order = lambda do |a, b|
-        if send(a.name) == nil
-          send(b.name) == nil ? 0 : -1
-        elsif a.name.to_sym == :id
-          send(b.name) == nil ? 1 : -1
-        else
-          (b.name.to_sym == :id || send(b.name) == nil) ? 1 : 0
-        end
-      end
-
       xml.tag!(qualifier, element_name.to_sym, attributes) do
-        xml_field_names.map { |field_name| get_field!(field_name) }.sort(&fields_order).each do |field|
+        xml_field_names.map { |field_name| get_field!(field_name) }.sort(&method(:fields_order)).each do |field|
           field.build_xml(xml, soap, send(field.name), options)
         end
+      end
+    end
+
+    def fields_order(a, b)
+      if send(a.name) == nil
+        send(b.name) == nil ? 0 : -1
+      elsif a.name.to_sym == :id
+        send(b.name) == nil ? 1 : -1
+      else
+        (b.name.to_sym == :id || send(b.name) == nil) ? 1 : 0
       end
     end
 
