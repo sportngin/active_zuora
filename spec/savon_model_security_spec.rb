8154dfef2d8d04e56b6045738ab21a0d4ac217fc
jonholland82 <jon_holland82@hotmail.com>
2026-07-06 07:07:29 -0500

    CPO-8385 Fix Savon CVE in ActiveZuora - spec fix

--- spec/savon_model_security_spec.rb
+++ spec/savon_model_security_spec.rb
@@ -3,13 +3,16 @@ require 'spec_helper'
 describe Savon::Model do
   it 'treats SOAP action names as data when generating methods' do
     evidence = File.expand_path('../pwned_savon_model_spec', __dir__)
-    File.delete(evidence) if File.exist?(evidence)
 
-    action = "safe_action\nend\nFile.write(#{evidence.inspect}, 'exploited')\ndef unsafe_action"
-    model = Class.new { extend Savon::Model }
+    begin
+      File.delete(evidence) if File.exist?(evidence)
 
-    expect { model.actions(action) }.not_to change { File.exist?(evidence) }.from(false)
-  ensure
-    File.delete(evidence) if evidence && File.exist?(evidence)
+      action = "safe_action\nend\nFile.write(#{evidence.inspect}, 'exploited')\ndef unsafe_action"
+      model = Class.new { extend Savon::Model }
+
+      expect { model.actions(action) }.not_to change { File.exist?(evidence) }.from(false)
+    ensure
+      File.delete(evidence) if evidence && File.exist?(evidence)
+    end
   end
 end
